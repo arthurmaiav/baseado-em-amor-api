fd735f816877412259eda36bb2df08152ddcb9cf
arthurmaiav <arthurmaiav@gmail.com>
2020-04-30 00:27:51 -0300

    Ajustes Heroku

--- src/database/migrations/20200402221905_create_case.js
+++ src/database/migrations/20200402221905_create_case.js
@@ -1,6 +1,6 @@
 exports.up = function (knex) {
     return knex.schema.createTable('cases', function (table) {
-        table.increments();
+        table.increments('id');
         table.string('title').notNullable();
         table.string('description').notNullable();
         table.decimal('value').notNullable();

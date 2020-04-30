56e54517c8857c41a449feff1eb188b70f3baf58
arthurmaiav <arthurmaiav@gmail.com>
2020-04-29 23:32:29 -0300

    Ajustes Heroku

--- knexfile.js
+++ knexfile.js
@@ -42,15 +42,6 @@ module.exports = {
 
   production: {
     client: 'pg',
-    connection: {
-      database: process.env.DATABASE_URL,
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
+    connection: process.env.DATABASE_URL,
   }
 };

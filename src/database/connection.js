aed2b5851e064cc35a930eb2d95af4d64f0c6ae3
arthurmaiav <arthurmaiav@gmail.com>
2020-04-29 22:47:46 -0300

    Ajuste Heroku

--- src/database/connection.js
+++ src/database/connection.js
@@ -1,7 +1,8 @@
 const knex = require('knex');
 const configuration = require('../../knexfile');
 
-const config = process.env.NODE_ENV == 'test' ?  configuration.test : configuration.development; 
+/* const config = process.env.NODE_ENV == 'test' ?  configuration.test : configuration.development; */
+const config = process.env.NODE_ENV == 'test' ?  configuration.test : configuration.production;
 
 const connection = knex(config);
 

268aa2eacae4e4b9d124f853efa5120270df4bea
arthurmaiav <arthurmaiav@gmail.com>
2020-04-30 00:57:33 -0300

    Ajustes Heroku

--- knexfile.js
+++ knexfile.js
@@ -43,6 +43,10 @@ module.exports = {
   production: {
     client: 'postgres',
     connection: process.env.DATABASE_URL,
+    pool: {
+      min: 2,
+      max: 10
+    },
     migrations: {
       directory: "./src/database/migrations"
     },

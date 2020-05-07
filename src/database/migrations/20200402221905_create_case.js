dc2495defc267b5def8b01dc1ac746523087bd3e
arthurmaiav <arthurmaiav@gmail.com>
2020-05-06 21:41:26 -0300

    Ajustes no banco e paginação

--- src/database/migrations/20200402221905_create_case.js
+++ src/database/migrations/20200402221905_create_case.js
@@ -2,7 +2,7 @@ exports.up = function (knex) {
     return knex.schema.createTable('cases', function (table) {
         table.increments('id');
         table.string('title').notNullable();
-        table.string('description').notNullable();
+        table.text('description').notNullable();
         table.decimal('value').notNullable();
         table.string('donationUrl');
     });
@@ -11,4 +11,3 @@ exports.up = function (knex) {
 exports.down = function(knex) {
   return knex.schema.dropTable('cases');
 };
-    
